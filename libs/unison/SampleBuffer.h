3989c5146dd1a412b469764482f138d490822fa6
kizzobot <kizzobot@gmail.com>
2010-06-18 21:07:36 -0700

    Began work on SampleBuffer class.
    1. Added SampleBuffer.h file to libs/unison .
    2. Modified libs/unison/CMakeLists.txt to reflect change.

---
+++ libs/unison/SampleBuffer.h
@@ -0,0 +1,18 @@
+#ifndef UNISON_SAMPLE_BUFFER_H
+#define UNISON_SAMPLE_BUFFER_H
+
+namespace Unison
+{
+
+class SampleBuffer
+{
+private:
+    void* m_data;
+    int m_frames;
+}
+
+}
+
+#endif
+
+// vim: ts=8 sw=2 sts=2 et sta noai
